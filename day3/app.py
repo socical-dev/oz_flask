4bafc6e55fee80ff101dac2451aa61571c240091
socical <socical.dev@gmail.com>
2025-02-26 18:14:32 +0900

    🌱 DB 연결 로직 개선
    db.yaml 파일 안에 DB 설정 정보 저장

--- day3/app.py
+++ day3/app.py
@@ -12,10 +12,16 @@ from flask_smorest import Api
 from flask_sqlalchemy import SQLAlchemy
 from db import db
 from models import User, Board
+import yaml
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:127.0.0.1@localhost/oz'
+with open('./db.yaml', 'r') as f:
+    config = yaml.safe_load(f)  # 파일 가져오기
+    
+# 데이터베이스 연결 정보
+database_config = config.get('database', {})
+app.config['SQLALCHEMY_DATABASE_URI'] = database_config.get('uri')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
